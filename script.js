4986d6138a3ec543013fe3db1616581aca604be2
hz-zh <115835509+hz-zh@users.noreply.github.com>
2023-01-10 19:52:06 -0500

    Adjust scoreboard to align scores under names
    Also remove commented code.

--- script.js
+++ script.js
@@ -98,19 +98,3 @@ function resetGame() {
 }
 
 game();
-
-/*const newspaperSpinning = [
-   { transform: 'rotate(0) scale(1)' },
-   { transform: 'rotate(360deg) scale(0)' }
- ];
- 
- const newspaperTiming = {
-   duration: 2000,
-   iterations: 1,
- }
- 
- const newspaper = document.querySelector(".newspaper");
- 
- newspaper.addEventListener('click', () => {
-   newspaper.animate(newspaperSpinning, newspaperTiming);
- }); */
